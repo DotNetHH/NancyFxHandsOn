9799c2be53c3fddb2a26457e66031d6da51d454f
Martin Johns <martin.johns@live.de>
2014-12-02 22:17:42 +0100

    Added status code handler to deliver a custom 404 response

---
+++ Nancy.DemoApplication1/NotFoundStatusCodeHandler.cs
@@ -0,0 +1,19 @@
+﻿using Nancy.ErrorHandling;
+using Nancy.Responses;
+
+namespace Nancy.DemoApplication1
+{
+    // No need to register this file, Nancy will find all IStatusCodeHandler instances
+    public class NotFoundStatusCodeHandler : IStatusCodeHandler
+    {
+        public bool HandlesStatusCode(HttpStatusCode statusCode, NancyContext context)
+        {
+            return statusCode == HttpStatusCode.NotFound;
+        }
+
+        public void Handle(HttpStatusCode statusCode, NancyContext context)
+        {
+            context.Response = new TextResponse("Whoops, could not find the page!");
+        }
+    }
+}
