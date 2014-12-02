37b0af13f4f3984f9f709da7b31063ece4e59719
Martin Johns <martin.johns@live.de>
2014-12-02 22:07:27 +0100

    Added method to trace all requests

--- Nancy.DemoApplication1/Bootstrapper.cs
+++ Nancy.DemoApplication1/Bootstrapper.cs
@@ -1,11 +1,21 @@
-﻿namespace Nancy.DemoApplication1
-{
-    using Nancy;
+﻿using System.Diagnostics;
+using Nancy.Bootstrapper;
+using Nancy.TinyIoc;
 
+namespace Nancy.DemoApplication1
+{
     public class Bootstrapper : DefaultNancyBootstrapper
     {
         // The bootstrapper enables you to reconfigure the composition of the framework,
         // by overriding the various methods and properties.
         // For more information https://github.com/NancyFx/Nancy/wiki/Bootstrapper
+        public Bootstrapper()
+        {
+        }
+
+        protected override void RequestStartup(TinyIoCContainer container, IPipelines pipelines, NancyContext context)
+        {
+            Trace.WriteLine(string.Format("Nancy request: {0} {1}", context.Request.Method, context.Request.Url));
+        }
     }
 }
