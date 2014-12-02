adba44bbedfb9f2b94a12f39257ac5907d77b99d
Kjell Otto <otto.kjell@gmail.com>
2014-12-02 20:30:14 +0000

    added example crud module and view

---
+++ NancyFxHandsOn.CRUD/CrudModule.cs
@@ -0,0 +1,25 @@
+﻿
+using Nancy;
+
+namespace NancyFxHandsOn.CRUD
+{
+    public class CrudModule : NancyModule
+    {
+        /// <summary>
+        /// Your CRUD Module with empty methods:
+        /// 
+        /// </summary>
+        public CrudModule()
+            : base("crud")
+        {
+            // Create
+            Post["/"] = _ => View["Views/crud"];
+            // Read
+            Get["/"] = _ => View["Views/crud"];
+            // Update
+            Put["/"] = _ => View["Views/crud"];
+            // Delete
+            Delete["/"] = _ => View["Views/crud"];
+        }
+    }
+}
